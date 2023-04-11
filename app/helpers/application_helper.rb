0375ff546cb175cd7c89c49993fb5dcaf3b6b953
super-man0115 <rikuto0115_73@icloud.com>
2023-04-12 01:23:26 +0900

    Add ogpの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'Deskography',
+      title: 'Deskography',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'Make Your Desk! Make Your Happy!',
+      keyword: 'デスク, ガジェット, エンジニア, プログラマー, デスクツアー, リモートワーク, デスクワーク',
+      canonical: request.original_url,
+      separator: '|',
+      icon: image_url('main_logo1.svg'),
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        locale: 'ja_JP',
+        image: image_url('ogp.png'),
+      },
+      twitter: {
+        card: 'summary_large_image',
+        image: image_url('ogp.png'),
+      }  
+    }
+  end  
 end
