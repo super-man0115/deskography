c2b6683d0ecbb01c05235e19cb4072ae11704eb0
super-man0115 <rikuto0115_73@icloud.com>
2023-04-28 00:00:02 +0900

    Add ゲストログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+  post "guest_login", to: "user_sessions#guest_login"
   resources :users, only: %i[new create show edit update]
   resources :posts do
     resources :comments, only: %i[create destroy update], shallow: true
