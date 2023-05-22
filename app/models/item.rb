2d8199466f74cecfc4b3987c3b18848828657cbd
super-man0115 <rikuto0115_73@icloud.com>
2023-05-23 00:29:57 +0900

    Add ransackでアイテム検索機能を追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,12 @@ class Item < ApplicationRecord
   has_many :users, through: :user_items
 
   validates :item_code, presence: true
+
+  def self.ransackable_associations(_auth_object = nil)
+    super + ['items']
+  end
+
+  def self.ransackable_attributes(_auth_object = nil)
+    ["created_at", "name", "updated_at", "caption", "maker"]
+  end
 end
